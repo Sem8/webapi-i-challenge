4949211a8bbee992a0fdad54ce7e9937dd2563cc
Sem8 <mssemmi8@gmail.com>
2019-03-19 02:47:26 -0400

    few minor changes with cors package added but not used

--- react-node/src/App.js
+++ react-node/src/App.js
@@ -5,10 +5,13 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
-    name: 'Jane Porter',
-    bio: 'real Tarzan\'s wife',
-    created_at: Date.now().toString(),
-    updated_at: Date.now().toString()
+    user: {
+      name: 'Jane Porter',
+      bio: 'real Tarzan\'s wife',
+      // created_at: Date.now().toString(),
+      // updated_at: Date.now().toString(),
+      // id: Math.random
+    }    
   };
 
   componentDidMount() {
@@ -26,9 +29,14 @@ class App extends Component {
     // .catch(err => console.error(err))
   };
 
-  addUser = () => {
-
-  }
+  // addUser = () => {
+  //   const { user } = this.state;
+  //   fetch(`http://localhost:4000/api/users/${user.id}`)
+  //   // .then(res => res.json())
+  //   .then(this.getUsers)    
+  //   .catch(err => console.error(err))
+  // };
+  
 
 
   renderUser = ({ id, name, bio }) => <div key={id}>{name} , {bio}</div>
@@ -40,17 +48,20 @@ class App extends Component {
 
         <div>
           <input
-            value={user.name}
+            value={user.name} 
+            onChange={e => this.setState({ user: {...user, name: e.target.value} })}
            />
            <input
             value={user.bio}
+            onChange={e => this.setState({ user: {...user, bio: e.target.value} })}
            />
-           <input
+           {/* <input
             value={user.created_at}
            />
            <input
             value={user.updated_at}
-           />
+           /> */}
+           {/* <button onClick={this.addUser}>Add user</button> */}
         </div>
         
       </div>
